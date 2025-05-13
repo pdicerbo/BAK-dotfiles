cbf8435424177ffa779ca8fe9ee446ee4e966e83
Pierluigi Di Cerbo <pierluigi.dicerbo@gmail.com>
2025-05-13 22:57:45 +0200

    mini-pairs moved into editor.lua; mini.map, mini.move and mini.surround added

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -63,5 +63,50 @@ return {
                 end,
             })
         end,
+    },
+
+    {
+        'echasnovski/mini.move', version = '*',
+        config = function()
+            require('mini.move').setup()
+        end
+
+     },
+
+    {
+        'echasnovski/mini.map', version = '*',
+
+        config = function()
+            require('mini.map').setup()
+        end
+
+    },
+
+    {
+
+        "echasnovski/mini.pairs",
+        event = "VeryLazy",
+        opts = {
+            modes = { insert = true, command = true, terminal = false },
+            -- skip autopair when next character is one of these
+            skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+            -- skip autopair when the cursor is inside these treesitter nodes
+            skip_ts = { "string" },
+            -- skip autopair when next character is closing pair
+            -- and there are more closing pairs than opening pairs
+            skip_unbalanced = true,
+            -- better deal with markdown code blocks
+            markdown = true,
+        },
+        config = function(_, opts)
+            require('mini.pairs').setup(opts)
+        end,
+    },
+
+    {
+         'echasnovski/mini.surround', version = '*',
+        config = function()
+            require('mini.surround').setup()
+        end
     }
 }

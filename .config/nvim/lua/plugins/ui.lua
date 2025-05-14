4aa7854f3ca13b4c6e60618e7b37d5af6510c648
Pierluigi Di Cerbo <pierluigi.dicerbo@gmail.com>
2025-05-14 12:02:31 +0200

    noice.nvim introduced; start plugins refactoring

---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,108 @@
+return {
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- add any options here
+        },
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        },
+        config = function()
+            require("noice").setup({
+                lsp = {
+                    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                    override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                    },
+                },
+                -- you can enable a preset for easier configuration
+                presets = {
+                    bottom_search = true, -- use a classic bottom cmdline for search
+                    command_palette = true, -- position the cmdline and popupmenu together
+                    long_message_to_split = true, -- long messages will be sent to a split
+                    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                    lsp_doc_border = false, -- add a border to hover docs and signature help
+                },
+            })
+        end
+    },
+
+    {
+        "akinsho/bufferline.nvim",
+        event = "VeryLazy",
+        keys = {
+            { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+            { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+            { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+            { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+            { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+            { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+            { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+            { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+            { "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+            { "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+        },
+        opts = {
+            options = {
+                -- stylua: ignore
+                close_command = function(n) Snacks.bufdelete(n) end,
+                -- stylua: ignore
+                right_mouse_command = function(n) Snacks.bufdelete(n) end,
+                diagnostics = "nvim_lsp",
+                always_show_bufferline = false,
+                -- diagnostics_indicator = function(_, _, diag)
+                --   local icons = LazyVim.config.icons.diagnostics
+                --   local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+                --     .. (diag.warning and icons.Warn .. diag.warning or "")
+                --   return vim.trim(ret)
+                -- end,
+                offsets = {
+                    {
+                        filetype = "neo-tree",
+                        text = "Neo-tree",
+                        highlight = "Directory",
+                        text_align = "left",
+                    },
+                    {
+                        filetype = "snacks_layout_box",
+                    },
+                },
+                ---@param opts bufferline.IconFetcherOpts
+                -- get_element_icon = function(opts)
+                --   return LazyVim.config.icons.ft[opts.filetype]
+                -- end,
+            },
+        },
+        config = function(_, opts)
+            require("bufferline").setup(opts)
+            -- Fix bufferline when restoring a session
+            vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
+                callback = function()
+                    vim.schedule(function()
+                        pcall(nvim_bufferline)
+                    end)
+                end,
+            })
+        end,
+    },
+
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        config = function()
+            require('lualine').setup {
+                options = {
+                    theme = 'nord'
+                }
+            }
+        end
+    }
+}

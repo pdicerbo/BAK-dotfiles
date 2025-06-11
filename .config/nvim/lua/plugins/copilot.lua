451943ac895ba170b968c01144351be35b1728e6
Pierluigi Di Cerbo <pierluigi.dicerbo@gmail.com>
2025-06-11 12:30:15 +0200

    copilot and diffview configs improved

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -41,25 +41,26 @@ return {
             -- Set to true to assume that copilot is already mapped
             -- vim.g.copilot_assume_mapped = true
             -- -- Set workspace folders
-            -- vim.g.copilot_workspace_folders = "~/Projects"
-            --
+            vim.g.copilot_workspace_folders = { vim.fn.getcwd() }
+
             -- -- Setup keymaps
-            -- local keymap = vim.keymap.set
-            -- local opts = { silent = true }
+            local keymap = vim.keymap.set
+            local opts = { silent = true }
             --
             -- -- Set <C-y> to accept copilot suggestion
             -- keymap("i", "<C-y>", 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
             --
             -- -- Set <C-i> to accept line
-            -- keymap("i", "<C-i>", "<Plug>(copilot-accept-line)", opts)
-            --
+            keymap("i", "<C-i>", "<Plug>(copilot-accept-line)", opts)
+            keymap('i', '<C-l>', '<Plug>(copilot-accept-word)', opts)
+
             -- -- Set <C-j> to next suggestion, <C-k> to previous suggestion, <C-l> to suggest
-            -- keymap("i", "<C-j>", "<Plug>(copilot-next)", opts)
-            -- keymap("i", "<C-k>", "<Plug>(copilot-previous)", opts)
-            -- keymap("i", "<C-l>", "<Plug>(copilot-suggest)", opts)
+            keymap("i", "<C-j>", "<Plug>(copilot-next)", opts)
+            keymap("i", "<C-k>", "<Plug>(copilot-previous)", opts)
+            keymap("i", "<C-h>", "<Plug>(copilot-suggest)", opts)
             --
             -- -- Set <C-d> to dismiss suggestion
-            -- keymap("i", "<C-d>", "<Plug>(copilot-dismiss)", opts)
+            keymap("i", "<C-d>", "<Plug>(copilot-dismiss)", opts)
         end,
     },
 
@@ -95,7 +96,7 @@ return {
                 -- Use tab for completion
                 complete = {
                     detail = "Use @<Tab> or /<Tab> for options.",
-                    insert = "<Tab>",
+                    insert = "<S-Tab>",
                 },
                 -- Close the chat
                 close = {
--- .config/nvim/lua/plugins/gitstuff.lua
+++ .config/nvim/lua/plugins/gitstuff.lua
@@ -48,5 +48,23 @@ return {
 
     {
         "sindrets/diffview.nvim",
+        cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+        keys = {
+            { "<leader>dv", "<cmd>DiffviewOpen<cr>", desc = "Open Diff View" },
+            { "<leader>dh", "<cmd>DiffviewFileHistory %<cr>", desc = "Open Diff View file history" },
+            { "<leader>dc", "<cmd>DiffviewClose<cr>", desc = "Close Diff View" },
+        },
+        config = function()
+            local actions = require("diffview.actions")
+            require("diffview").setup({
+                enhanced_diff_hl = true,
+                keymaps = {
+                    file_panel = {
+                        { "n",  "u",     actions.scroll_view(-0.25), { desc = "Scroll the view up" } },
+                        { "n",  "d",     actions.scroll_view(0.25),  { desc = "Scroll the view down" } },
+                    },
+                }
+            })
+        end,
     },
 }
